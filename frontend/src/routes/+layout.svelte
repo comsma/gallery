fcf1da6b6213ce9f103186bf0ab69e5bcde168a5
Collin Smallegan <cmsmallegan@comsma.com>
2023-11-03 23:57:22 -0500

    updated mobile view and logo

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,10 +2,15 @@
     import "../app.css"
 </script>
 
-<div class="flex pt-10 mx-5">
-    <aside class="h-screen sticky top-0">
-        <div class="max-w-[800px]">
-            <img src="/COMSMA.webp">
+<div class="flex pt-10 mx-5 flex-col lg:flex-row">
+    <div class="block lg:hidden">
+        <div class="mx-auto object-center">
+            <img height="auto" width="300px" class="m-auto" alt="comsma logo" src="/COMSMA.webp">
+        </div>
+    </div>
+    <aside class="hidden h-screen sticky top-0 lg:block">
+        <div class="">
+            <img height="auto" width="1000px" alt="comsma logo" src="/COMSMA.webp">
         </div>
         <div class="text-sm text-slate-600">
             <a href="https://comsma.com">about me</a>
